a492aa38ebe24aeefdfc3dd349e750c96ab8b794
Mikah <graywolf.niehaus@live.com>
2025-02-17 15:52:24 -0600

    added to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000', 'https://tasklist-frontend.pages.dev'  # ✅ Allow both local and deployed frontend
-  
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :patch, :put, :delete]
+      origins '*'  # Allow requests from anywhere (or specify your frontend URL)
+      resource '*', 
+        headers: :any, 
+        methods: [:get, :post, :patch, :put, :delete, :options]
     end
   end
   
