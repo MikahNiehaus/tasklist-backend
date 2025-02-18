1b80cbd542c41101624937490d505b93c3875f60
Mikah <graywolf.niehaus@live.com>
2025-02-17 23:45:13 -0600

    h

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module TasklistBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load = ENV['RAILS_ENV'] == 'production'
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
