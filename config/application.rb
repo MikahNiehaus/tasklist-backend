415bdf26494a8899c45faebdeee6825e78adc306
Mikah <graywolf.niehaus@live.com>
2025-02-18 09:30:29 -0600

    secret

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ module TasklistBackend
   class Application < Rails::Application
     # Initialize configuration defaults for the originally generated Rails version.
     config.load_defaults 8.0
-    config.secret_key_base = ENV['SECRET_KEY_BASE'] || 'dummy_secret_key'
+    config.secret_key_base = ENV['SECRET_KEY_BASE']
 
     # Ignore `lib/assets`, `lib/tasks`, or anything that doesn't contain `.rb` files
     config.autoload_lib(ignore: %w[assets tasks])
